417af31d622a53e8b00f59cbbd72aa3fc0e14eec
haerxeong <haeryeong04@naver.com>
2024-10-04 05:01:43 +0900

    mission2

--- UMC/2/mission2/src/App.jsx
+++ UMC/2/mission2/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { MOVIES } from "./mocks/movies";
+import "./App.css";
 
 const MovieGrid = () => {
   const imageBaseUrl = "https://image.tmdb.org/t/p/w500";
@@ -7,9 +8,14 @@ const MovieGrid = () => {
   return (
     <div className="movie-grid">
       {MOVIES.results.map((movie) => (
-        <div key={movie.id} className="movie-item">
+        <div
+          key={movie.id}
+          className="movie-item"
+          style={{ position: "relative" }}
+        >
           <img src={`${imageBaseUrl}${movie.poster_path}`} alt={movie.title} />
-          <h3>{movie.title}</h3>
+          <div className="overlay"></div> {/* 오버레이 요소 추가 */}
+          {/* <h3>{movie.title}</h3> */}
         </div>
       ))}
     </div>
