90646d31acf99d83ce5d8ee1b16e71566b6beaf9
haerr <144093044+haerxeong@users.noreply.github.com>
2024-10-25 05:07:15 +0900

    Delete JavaScriptWorkspace/BasicStudy directory

--- JavaScriptWorkspace/BasicStudy/BOJ8393.js
+++
@@ -1,8 +0,0 @@
-//const [n] = require('fs').readFileSync(0, 'utf-8').map(Number);
-const n = Number(require("fs").readFileSync("/dev/stdin").toString());
-
-let sum = 0;
-
-for (let i = 1; i <= n; i++) sum += i;
-
-console.log(sum);
